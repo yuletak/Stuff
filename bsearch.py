83be38c55e85d126b681e30aedbf82a7f1237dbe
hadmin <hadmin@qa-core.(none)>
2013-03-08 00:53:22 -0800

    Removed commented out lines of code

--- bsearch.py
+++ bsearch.py
@@ -23,24 +23,6 @@ for i in range(length):
 start = 0
 end = length-1
 
-#if target == mylist[end]:
-#    print 'index found:  {0}'.format(end)
-#elif target == mylist[start]:
-#    print 'index found:  {0}'.format(start)
-#elif target > mylist[end] or target < mylist[start]:
-#    print 'target cannot be found'
-#else:    
-#    while start < end:
-#        i = (end - start)/2 + start
-#        print 'start:  {0}  end:  {1}  index:  {2}'.format(start, end, i)
-#        if target == mylist[i]:
-#            print 'index found:  {0}'.format(i)
-#            break
-#        elif target > mylist[i]:
-#            start = i+1
-#        elif target < mylist[i]:
-#            end = i-1
-
 while start <= end:
     i = (end + start)/2
     print 'start:  {0}  end:  {1}  index:  {2}'.format(start, end, i)
